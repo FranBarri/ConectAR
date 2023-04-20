fbf84a17816aaea31456a5c6c9a38d21d20cfc45
matiaspsara <matiaspsara@gmail.com>
2023-04-20 16:21:41 -0300

    Creadas las clases Aplicacion, Conexion, Grafo, Localidad, ConjuntoDisjunto, Kruskal en el paquete logica

---
+++ src/logica/Conexion.java
@@ -0,0 +1,30 @@
+package logica;
+
+public class Conexion implements Comparable<Conexion> {
+    private Localidad origen;
+    private Localidad destino;
+    private double costo;
+
+    public Conexion(Localidad origen, Localidad destino, double costo) {
+        this.origen = origen;
+        this.destino = destino;
+        this.costo = costo;
+    }
+
+    public Localidad getOrigen() {
+        return origen;
+    }
+
+    public Localidad getDestino() {
+        return destino;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    @Override
+    public int compareTo(Conexion otraArista) {
+        return Double.compare(this.costo, otraArista.getCosto());
+    }
+}
